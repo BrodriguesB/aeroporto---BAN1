a8ccfcef0933efbbc83d94df2c6a1dc95bd0e838
Gabriel Rohden <gabriel_rohden@outlook.com>
2017-12-05 12:53:47 -0200

    Ignores, data and database models

---
+++ models/data.js
@@ -0,0 +1,120 @@
+const pg = require('pg');
+const connectionString = require('../configurations').getConnectionString;
+const client = new pg.Client(connectionString());
+
+const data = [
+    `
+    INSERT INTO pais
+    VALUES(1,'Alemanha','Berlin','Europa'),
+        (2,'Brasil','Brasilia','America'),
+        (3,'Canada','Ottawa','America'),
+        (4,'Estados Unidos','Whashington','America'),
+        (5,'Egito','Cairo','Africa'),
+        (6,'Japao','Toquio','Asia'),
+        (7,'Australia','Camberra','Oceania'),
+        (8,'Holanda','Amsterda','Europa');
+
+    INSERT INTO estado
+    VALUES	('AC','Acre',2),
+        ('AL','Alagoas',2),
+        ('AP','Amapá',2),
+        ('AM','Amazonas',2),
+        ('BH','Bahia',2),
+        ('CE','Ceará',2),
+        ('DF','Distrito Federal',2),
+        ('ES','Espírito Santo',2),
+        ('GO','Goiás',2),
+        ('MA','Maranhão',2),
+        ('MT','Mato Grosso',2),
+        ('MS','Mato Grosso do Sul',2),
+        ('MG','Minas Gerais',2),	
+        ('PA','Pará',2),
+        ('PB','Paraíba',2),
+        ('PR','Paraná',2),
+        ('PE','Pernambuco',2),
+        ('PI','Piauí',2),
+        ('RJ','Rio de Janeiro',2),
+        ('RN','Rio Grande do Norte',2),
+        ('RS','Rio Grande do Sul',2),
+        ('RO','Rondônia',2),
+        ('RR','Roraima',2),
+        ('SC','Santa Catarina',2),
+        ('SP','São Paulo',2),
+        ('SE','Sergipe',2),
+        ('TO','Tocantins',2);
+
+    INSERT INTO aeroporto
+    VALUES('CWB','Aeroporto Afonso Pena','Curitiba',true,'PR'),
+        ('CGH','Aeroporto de Congonhas','Sao Paulo',true,'SP'),
+        ('GIG','Aeroporto do Galeão','Rio de Janeiro',false, 'RJ'),
+        ('SDU','Aeroporto Santos Dumont','Rio de Janeiro',true,'RJ'),
+        ('CNF','Aeroporto de Confins','Belo Horizonte',true,'MG');
+        
+    INSERT INTO companhia
+    VALUES (1,'Latam'),
+        (2,'Gol'),
+        (3,'Avianca'),
+        (4,'Azul'),
+        (5,'Air France'),
+        (6,'Emirates'),
+        (7,'Delta'),
+        (8,'TAP Portugal'),
+        (9,'American AirLines'),
+        (10,'Continental AirLines');
+
+    INSERT INTO modelo
+        VALUES 	(1,'A380','AirBus',550),
+            (2,'777','Boeing',555),
+            (3,'A320','AirBus',220),
+            (4,'A319','AirBus',220),
+            (5,'737','Boeing',215),
+            (6,'747','Boeing',416),
+            (7,'787','Boeing',420),
+            (8,'E195-E2','Embraer',110);
+
+    INSERT INTO aeronave
+        VALUES 	(123,3,4),
+            (234,1,5),
+            (201,2,3),
+            (318,8,7);
+
+    INSERT INTO voo
+    VALUES 	(001,'Sao Paulo/Curitiba','15:10','2017/12/15','00:40',123),
+        (002,'Porto Alegre/Rio de Janeiro','18:35','2017/12/20','02:30',234),
+        (004,'Sao Paulo/Madri','06:30','2017/12/01','07:00',318);    
+    
+    INSERT INTO aeroporto_companhia
+    VALUES	(3,'CGH'),
+        (4,'CWB'),
+        (1,'SDU');
+        
+    INSERT INTO bagagem
+    VALUES 	(01,18.000,TRUE),
+        (02,15.000,FALSE),
+        (03,17.300,TRUE);
+
+    INSERT INTO passageiros
+    VALUES ('19337264749','ALESSANDRA AZEVEDO DA COSTA',true,'economica',NULL,984575412,001,01),
+        ('42159270130','ANTONIO FURTADO NUNES',false,'executiva',NULL,99932010178,002,02),
+        ('41234653826','DANIEL PEREIRA COSTA',true,'economica',NULL,9715484765,001,03),
+        ('73896802453','KARINA DE CASSIA MAIA',true,'vip',NULL,99645786598,001,null),
+        ('27749159886','JEFFERSON MACHADO PEREIRA',false,'economica',NULL,98845781203,002,null),
+        ('64610764920','SAMARA BRITO DO NASCIMENTO ',true,'especial','cadeirante',956457812,001,null),
+        ('74658379287','NATALIA CIBELLE DA SILVA BARBOSA ',true,'executiva',NULL,99954612320,002,null);
+    
+    INSERT INTO tripulacao
+    VALUES	('41514050200','JACKSON FERREIRA PEREIRA','1980/05/04','2005/01/20','Rua 1200',470,'Barra da Tijuca','Rio de Janeiro','RJ','99687451232',001,'piloto',8000),
+        ('34181954234','JEAN PHABLO SANTOS DE SOUZA','1985/07/28','2010/06/13','Rua 90',100,'Vilha Velha','Osasco','SP','987653214',001,'copiloto',4500),
+        ('57372110297' , 'MARCIA ANDREIA COLARES PINHEIRO','1987/10/15','2012/03/17','Rua 920',550,'Centro','Florianopolis','SC','99145786230,',001,'comissária',2200);
+    
+    INSERT INTO habilitacao
+    VALUES  (2500,'41514050200',4)
+    `
+];
+execQuery(data.join(''))
+
+async function execQuery(query){
+    client.connect();
+    await client.query(query); //TODO: exec one by one?
+    client.end();
+}
--- models/database.js
+++ models/database.js
@@ -1,90 +1,111 @@
-(function() {
-    'strict mode';
-    const pg = require('pg');
+const pg = require('pg');
+const connectionString = require('../configurations').getConnectionString;
+const client = new pg.Client(connectionString());
 
-    const userName = "postgres";
-    const password = "adm";
-    const host = "localhost";
-    const dbName = "aeroporto";
-    const connectionDBStr = 'postgres://' + userName + ':' + password + '@' + host + ':5432/' + dbName;
-    const client = new pg.Client(connectionDBStr);
 
-    const tables = [
-        `
-        create table modelo_aviao(
-            id_modelo_aviao numeric(11),
-            cod_modelo_aviao character varying(50),
-            num_capacidade_passageiros numeric(5),
-            qtd_peso numeric(6,2),
-            num_maximo_aeroporto numeric(3),
-            primary key (id_modelo_aviao)
-        )`,
-        `create table aviao(
-            id_aviao numeric(11),
-            id_modelo_aviao numeric(11),
-            primary key (id_aviao),
-            foreign key (id_modelo_aviao) references modelo_aviao(id_modelo_aviao)
-        )`,
-        `create table sindicato(
-            id_sindicato numeric(11),
-            den_sindicato character varying(50),
-            primary key (id_sindicato)
-        )`,
-        `create table cargo(
-            id_cargo numeric(11),
-            den_cargo character varying(50),
-            primary key(id_cargo)
-        )`,
-        `create table funcionario(
-            num_matricula numeric(11),
-            nom_funcionario character varying(50),
-            den_endereco character varying(50),
-            num_telefone numeric(15),
-            val_salario numeric(8,2),
-            id_cargo numeric(11),
-            num_membro_sindicato numeric(11),
-            id_sindicato numeric(11),
-            primary key (num_matricula),
-            foreign key(id_cargo) references cargo(id_cargo),
-            foreign key(id_sindicato) references sindicato(id_sindicato)
-        )`,
-        `create table habilidades(
-            id_funcionario numeric(11),
-            id_modelo numeric(11),
-            primary key (id_funcionario, id_modelo),
-            foreign key (id_funcionario) references funcionario(num_matricula),
-            foreign key (id_modelo) references modelo_aviao(id_modelo_aviao)
-        )`,
-        `create table exame_medico(
-            id_exame_medico numeric(15),
-            id_funcionario numeric(11),
-            dat_exame date,
-            primary key (id_exame_medico),
-            foreign key (id_funcionario) references funcionario(num_matricula)
-        )`,
-        `create table teste_principal(
-            id_teste_principal numeric(11),
-            cod_teste_anac character varying(50),
-            den_teste character varying(50),
-            val_pontuacao_maxima numeric(4),
-            primary key (id_teste_principal)
-        )`,
-        `create table teste_aviao(
-            id_teste_aviao numeric(20),
-            id_teste_principal numeric(11),
-            id_aviao numeric(11),
-            dat_teste date,
-            id_funcionario numeric(11),
-            val_pontuacao_aviao numeric(4),
-            primary key (id_teste_aviao),
-            foreign key (id_teste_principal) references teste_principal(id_teste_principal),
-            foreign key (id_aviao) references aviao(id_aviao),
-            foreign key (id_funcionario) references funcionario(num_matricula)
-        )`
-    ];
+const tables = [
+    `
+    CREATE TABLE pais(
+        id_pais int PRIMARY KEY,
+        nome varchar (20) NOT NULL,
+        capital varchar (20),
+        continente varchar (20)
+    );	
+    
+    CREATE TABLE estado(
+        sigla_estado varchar (5) PRIMARY KEY,
+        nome varchar (20) NOT NULL,
+        id_pais int REFERENCES pais (id_pais)
+    );
+    
+    CREATE TABLE aeroporto(
+        sigla varchar(5)PRIMARY KEY UNIQUE,
+        nome varchar(50),
+        cidade varchar(20)NOT NULL,
+        status boolean,
+        sigla_estado varchar(5) REFERENCES estado (sigla_estado)
+    );
+        
+    
+    CREATE TABLE companhia(
+        id_companhia int PRIMARY KEY,
+        nome varchar (20) UNIQUE
+    );
+    
+    CREATE TABLE aeroporto_companhia(
+        id_companhia int REFERENCES companhia (id_companhia),
+        sigla varchar REFERENCES aeroporto (sigla),
+        PRIMARY KEY (sigla)
+    );
+        
+    
+    CREATE TABLE modelo(
+        id_modelo int PRIMARY KEY,
+        nome varchar (15) UNIQUE,
+        fabricante varchar(10),
+        capacidade int
+    );
+    
+    CREATE TABLE aeronave(
+        id_aeronave int PRIMARY KEY,
+        id_companhia int REFERENCES companhia (id_companhia),
+        id_modelo int REFERENCES modelo (id_modelo)
+    );
+        
+    CREATE TABLE voo(
+        id_voo int PRIMARY KEY,	
+        rota varchar(50) NOT NULL,
+        horario time NOT NULL,
+        data date NOT NULL,
+        duracao time NOT NULL,
+        id_aeronave int REFERENCES aeronave (id_aeronave)
+    );
+    
+    CREATE TABLE passageiros(
+        cpf NUMERIC(11) PRIMARY KEY UNIQUE,
+        nome varchar (50) NOT NULL,
+        ckeckin boolean,
+        classe varchar,
+        necessidade varchar (200),
+        telefone varchar(15), 
+        id_voo int REFERENCES voo (id_voo),
+        id_bagagem int REFERENCES bagagem (id_bagagem)
+    );
+    
+    CREATE TABLE tripulacao(
+        cpf NUMERIC (11) PRIMARY KEY UNIQUE,
+        nome varchar (50) NOT NULL,
+        data_nasc date,
+        data_admissao date,
+        rua varchar(50),
+        numero int,
+        bairro varchar,
+        cidade varchar,
+        sigla_estado varchar(5) REFERENCES estado (sigla_estado),
+        telefone varchar(15), 
+        id_voo int REFERENCES voo (id_voo),
+        atividade varchar (20) NOT NULL,
+        hora_voo int
+    );
+    
+    CREATE TABLE bagagem(
+        id_bagagem int PRIMARY KEY,
+        peso float NOT NULL,
+        despacho boolean NOT NULL
+    );
+    
+    CREATE TABLE habilitacao(
+        tempo_voo int,
+        cpf numeric(11) REFERENCES tripulacao (cpf),
+        id_modelo int REFERENCES MODELO (id_modelo),
+        PRIMARY KEY (tempo_voo,id_modelo)
+    );
+    `
+];
+execQuery(tables.join(''))
+
+async function execQuery(query) {
     client.connect();
-    const query = client.query(tables.join('; ')); //TODO: exec one by one?
-    query.on('end', function () {
-        client.end();
-    });
-})();
+    await client.query(query); //TODO: exec one by one?
+    client.end();
+}
